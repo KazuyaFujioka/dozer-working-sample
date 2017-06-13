cc5043cfbf65beaf6fa62e3f2ce0a488f787b511
KazuyaFujioka <kazuya.fujioka@gmail.com>
2017-06-13 18:08:16 +0900

    不要メソッド削除

--- core/src/main/java/com/example/domain/model/fundamentals/PaymentMethod.java
+++ core/src/main/java/com/example/domain/model/fundamentals/PaymentMethod.java
@@ -2,10 +2,4 @@ package com.example.domain.model.fundamentals;
 
 public enum PaymentMethod {
     onsite, credit;
-
-    public static PaymentMethod create(String value)
-    {
-        System.out.print("call!!!");
-        return PaymentMethod.valueOf(value);
-    }
 }
